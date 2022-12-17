6d1d2a8c67d01d1c25f3d1d5194ecd5641ac3b3d
Sávio <savio.matheus.sousa@gmail.com>
2022-12-17 20:52:39 -0300

    wip: inicio da escrita dos testes de api

---
+++ backend/src/test/java/todoso/backend/OlarTest.java
@@ -0,0 +1,32 @@
+package todoso.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(
+	classes=todoso.backend.controlador.Todoso.class,
+	webEnvironment = WebEnvironment.RANDOM_PORT
+)
+class OlarTest {
+
+	@LocalServerPort
+	private int porta;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void primeiraInicializacao() {
+		assertThat(
+			this.restTemplate.getForObject(
+				"http://localhost:"+ porta +"/api/v1/olar", String.class
+			)
+		).contains("Olá, Mundo!");
+	}
+
+}
